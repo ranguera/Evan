914e6093777c93c18c33173d3982df6576a01253
Roger <ranguera@gmail.com>
2019-08-22 20:40:20 -0700

    last changes

---
+++ Assets/Scripts/ChooseCam.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChooseCam : MonoBehaviour
+{
+    public GameObject cam1, cam2, cam3;
+    public GameObject controller;
+    public float focalLength;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Alpha1))
+        {
+            cam1.SetActive(true);
+            cam2.SetActive(false);
+            cam3.SetActive(false);
+            cam1.transform.position = controller.transform.position;
+            Instantiate()
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(true);
+            cam3.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            cam1.SetActive(false);
+            cam2.SetActive(false);
+            cam3.SetActive(true);
+        }
+    }
+}
